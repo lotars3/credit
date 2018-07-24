8d39f92df9ae4562e3ba6838f0d209ecebc199b5
Oskix <oskar.polak@akademiakodu.pl>
2018-07-24 10:18:38 +0200

    sd

--- src/main/java/pl/oskarpolak/credit/controllers/MainController.java
+++ src/main/java/pl/oskarpolak/credit/controllers/MainController.java
@@ -2,10 +2,13 @@ package pl.oskarpolak.credit.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.oskarpolak.credit.models.forms.CreditForm;
+
+import javax.validation.Valid;
 
 @Controller
 public class MainController {
@@ -16,9 +19,13 @@ public class MainController {
     }
 
     @PostMapping("/")
-    public String index(@ModelAttribute("creditForm") CreditForm creditForm,
+    public String index(@ModelAttribute("creditForm") @Valid CreditForm creditForm,
+                        BindingResult bindingResult,
                         Model model){
-        System.out.println(creditForm);
+        if(bindingResult.hasErrors()){
+            model.addAttribute("someInfo", "Uzupełnij popranwie pola");
+            return "creditForm";
+        }
         if((creditForm.getSalary() - creditForm.getCosts()) * 0.30 > creditForm.getCreditValue() / creditForm.getCreditTime()){
              model.addAttribute("someInfo", "Udzielono kredytu");
              return "creditForm";
--- src/main/java/pl/oskarpolak/credit/models/forms/CreditForm.java
+++ src/main/java/pl/oskarpolak/credit/models/forms/CreditForm.java
@@ -2,11 +2,24 @@ package pl.oskarpolak.credit.models.forms;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 
 @Data
 public class CreditForm {
+    @Min(250)
+    @Max(1000000)
     private double creditValue;
+    @Min(1)
+    @Max(1200)
+    //@Pattern(regexp = "i lecisz regexem")
     private double creditTime;
+    @Min(0)
+    @Max(10000000)
     private double salary;
+    @Min(0)
+    @Max(10000000)
     private double costs;
 }
