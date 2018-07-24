fe12b7b6742b2c7a9e039a20188fed5c5cf2540a
Oskix <oskar.polak@akademiakodu.pl>
2018-07-24 11:43:30 +0200

    sd

--- src/main/java/pl/oskarpolak/credit/models/forms/CreditForm.java
+++ src/main/java/pl/oskarpolak/credit/models/forms/CreditForm.java
@@ -5,11 +5,12 @@ import lombok.Data;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 
 @Data
 public class CreditForm {
-    @Min(250)
+    @Min(250) 
     @Max(1000000)
     private double creditValue;
     @Min(1)
